88ed826b5fa1ed9b59d90b271f8583a6d0b69973
Wanderson Ferreira <iagwanderson@gmail.com>
2020-02-18 18:01:29 -0300

    style guide

--- project.clj
+++ project.clj
@@ -15,7 +15,6 @@
                  [seancorfield/next.jdbc "1.0.13"]
                  [com.taoensso/nippy "2.14.0"]
                  [org.clojure/test.check "0.10.0"]]
-
   :profiles {:dev {:source-paths ["src" "dev"]
                    :plugins [[refactor-nrepl "2.4.0"]
                              [cider/cider-nrepl "0.23.0"]]}}
